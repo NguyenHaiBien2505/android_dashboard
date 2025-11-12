87dd0c224e3a3f564d5b238dd483655b024a0567
NguyenHaiBien2505 <162653495+NguyenHaiBien2505@users.noreply.github.com>
2025-11-12 10:14:46 +0700

    dashboard

---
+++ app/src/main/java/com/he180659/dashboard1/MainActivity.java
@@ -0,0 +1,54 @@
+package com.he180659.dashboard1;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.PopupMenu;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView iconUser = findViewById(R.id.iconUser);
+
+        // Xử lý click icon user
+        iconUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showUserPopupMenu(v);
+            }
+        });
+    }
+
+    private void showUserPopupMenu(View view) {
+        PopupMenu popupMenu = new PopupMenu(this, view);
+        popupMenu.getMenuInflater().inflate(R.menu.user_menu, popupMenu.getMenu());
+
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                int id = item.getItemId();
+
+                if (id == R.id.menu_profile) {
+                    Toast.makeText(MainActivity.this, "Mở thông tin tài khoản", Toast.LENGTH_SHORT).show();
+                    return true;
+                } else if (id == R.id.menu_update) {
+                    Toast.makeText(MainActivity.this, "Cập nhật thông tin", Toast.LENGTH_SHORT).show();
+                    return true;
+                } else if (id == R.id.menu_logout) {
+                    Toast.makeText(MainActivity.this, "Đăng xuất", Toast.LENGTH_SHORT).show();
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        popupMenu.show();
+    }
+}
